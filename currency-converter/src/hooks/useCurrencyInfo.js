734833a975f52802d49994daa9712c0f3c4160a7
lsvishaal <lsvishaal7@gmail.com>
2024-07-15 23:51:33 +0530

    chore: Update currency API endpoint in useCurrencyInfo hook

--- currency-converter/src/hooks/useCurrencyInfo.js
+++ currency-converter/src/hooks/useCurrencyInfo.js
@@ -4,9 +4,17 @@ function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
 
   useEffect(() => {
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-      .then((res) => res.json())
-      .then((res) => setData(res[currency]));
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((res) => setData(res[currency]))
+      .catch((error) => {
+        console.error('Fetch error:', error.message);
+      });
   }, [currency]);
 
   return data;
